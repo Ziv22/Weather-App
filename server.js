0b8a0256e23fb5fac8a1bf8c42d53adbb82e5631
ziv221 <ziv.itzh@gmail.com>
2020-09-21 22:37:19 +0300

    added last minue changes

--- server.js
+++ server.js
@@ -3,13 +3,24 @@ const   api         = require("./server/routes/api.js"),
         express     = require("express"),
         path        = require('path'),
         app         = express(),
-        port        = 3000
+        mongoose    = require("mongoose")
+        port        = 7777
 
 app.use(express.static(path.join(__dirname, 'node_modules')))
 app.use(express.static(path.join(__dirname, 'dist')))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/',api)
+
+mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost:27017/CitiesDB' , {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    })
+    .then(() => console.log('DB Connected!'))
+    .catch(err => {
+    console.log(`DB Connection Error: ${err.message}`)
+    })
+
 
 
 app.listen(process.env.PORT || port, function(){
--- server/model/City.js
+++ server/model/City.js
@@ -1,12 +1,4 @@
 const mongoose  = require("mongoose")
-mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost:27017/CitiesDB' , {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    })
-    .then(() => console.log('DB Connected!'))
-    .catch(err => {
-    console.log(`DB Connection Error: ${err.message}`)
-    })
 
 const Schema    = mongoose.Schema
 
